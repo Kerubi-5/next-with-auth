24f72bce0e0e246fd273d79d13ef06e518dbdf4a
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-27 16:42:27 +0800

    styles: Footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Im a footer</div>;
+  return (
+    <div className="text-center">
+      <p>All rights reserved | KK &copy; 2022</p>
+    </div>
+  );
 };
 
 export default Footer;
