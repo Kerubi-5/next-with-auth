4c40b34f8a8cb71d07a259fc9e2d41ddb9323430
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-27 16:37:05 +0800

    styles: Navbar

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -3,16 +3,20 @@ import s from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <div>logo</div>
+    <header className={s.root}>
+      <nav className={s.navbar}>
+        <div className={s.logo}>
+          <Link href="/">
+            <a>KK</a>
+          </Link>
+        </div>
 
-        <div>
+        <div className={s.navMenu}>
           <Link href="/auth/login">
-            <a>Login</a>
+            <a className={s.navLink}>Login</a>
           </Link>
           <Link href="/auth/register">
-            <a>Register</a>
+            <a className={s.navLink}>Register</a>
           </Link>
         </div>
       </nav>
