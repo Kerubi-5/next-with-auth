fffc9889a99752b01e13174654d5a90fb18cb097
John Kim Querobines <querobinesj@gmail.com>
2022-04-29 12:24:52 +0800

    feat: adds all framework and component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import { Layout } from "@components/common";
-import { FC } from "react";
+import { InferGetServerSidePropsType } from "next";
+import { fetchApi } from "@framework/utils";
+import { TodoContainer } from "@components/todo";
+import TodoItem from "@components/todo/TodoItem/TodoItem";
+export async function getServerSideProps() {
+  const todos = await fetchApi();
+  return {
+    props: {
+      todos,
+    }, // will be passed to the page component as props
+  };
+}
 
-const Home = () => {
+const Home = ({
+  todos,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(todos);
+
   return (
     <>
       <div>
         <h1>Home</h1>
+        <TodoContainer>
+          <TodoItem />
+        </TodoContainer>
       </div>
     </>
   );
