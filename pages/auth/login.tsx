b9e4aba4f93f56ab2a536a7bd077117f54a79a1e
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-29 17:25:56 +0800

    fix: prevent default

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,19 +2,17 @@ import { Layout } from "@components/common";
 import { Input, Form } from "@components/form";
 import { Button } from "@components/ui";
 import { UilAt, UilLock } from "@iconscout/react-unicons";
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
 
 const Login = () => {
   const loginRef = useRef<HTMLFormElement>(null);
 
-  const handleSubmit = () => {
-    // Prevent submit default
-    if (loginRef.current) {
-      loginRef.current.preventDefault();
-    }
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
   };
+
   return (
-    <Form ref={loginRef} method="POST" onSubmit={handleSubmit}>
+    <Form ref={loginRef} method="POST" onSubmit={(e) => handleSubmit(e)}>
       <h1 className="text-center text-2xl font-bold">Login Form</h1>
       <Input label="Email" name="email" type="email" icon={UilAt} />
       <Input label="Password" name="password" type="password" icon={UilLock} />
