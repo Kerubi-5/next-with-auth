90be62662a6cf8fdc8be8036e898bdc95e1437ff
John Kim Querobines <querobinesj@gmail.com>
2022-04-29 11:17:09 +0800

    fix: removes red squiggly lines

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import { Layout } from "@components/common";
-import type { NextPage } from "next";
+import { FC } from "react";
 
 const Home = () => {
   return (
-    <div>
-      Hello
-      <span>User here: </span>
-      <h1 className="underline">SD</h1>
-      <h1 className="text-red-400">SDASDS</h1>
-    </div>
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
   );
 };
 
