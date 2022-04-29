8588e12587daee32fd43627badb36a26a2034c36
John Kim Querobines <querobinesj@gmail.com>
2022-04-29 13:38:16 +0800

    feat: Login and Register routes

---
+++ pages/auth/login.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "@components/common";
+
+const Login = () => {
+  return <div>Register</div>;
+};
+
+Login.Layout = Layout;
+
+export default Login;
---
+++ pages/auth/register.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "@components/common";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+Register.Layout = Layout;
+
+export default Register;
