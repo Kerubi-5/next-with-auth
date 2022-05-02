e35d79430968e733fcf0f8099eeac386286604e0
John Kim Querobines <querobinesj@gmail.com>
2022-05-02 10:40:44 +0800

    feat: renders todo items

--- components/todo/TodoItem/TodoItem.tsx
+++ components/todo/TodoItem/TodoItem.tsx
@@ -1,9 +1,14 @@
 import { UilPen, UilTrash } from "@iconscout/react-unicons";
 import s from "./TodoItem.module.css";
 import { Toggle } from "@components/ui";
-import { useState } from "react";
+import { useState, FC } from "react";
+import { Todo } from "@common/types/todo";
 
-const TodoItem = () => {
+interface ITodoItem {
+  todo: Todo;
+}
+
+const TodoItem: FC<ITodoItem> = ({ todo }) => {
   const [isComplete, setIsComplete] = useState(false);
 
   const strikeThrough = isComplete && "line-through";
@@ -17,11 +22,8 @@ const TodoItem = () => {
         />
       </div>
       <div className={`${s.details} ${strikeThrough}`}>
-        <h3 className={s.title}>Item</h3>
-        <p className={s.description}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-          quidem.
-        </p>
+        <h3 className={s.title}>{todo.title}</h3>
+        <p className={s.description}>{todo.description}</p>
       </div>
       <div className={s.buttons}>
         <button>{<UilPen className="text-[#60a5fa]" />}</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import { Layout } from "@components/common";
 import { InferGetServerSidePropsType } from "next";
 import { TodoCard, TodoItem } from "@components/todo";
 import { useHook } from "@framework/utils";
+import { Todo } from "@common/types/todo";
 export async function getServerSideProps() {
-  const todos = await useHook.todo.useTodo();
+  const todo = useHook.todo;
+
+  const todos = (await todo.useTodo()) as Todo[];
 
   return {
     props: {
@@ -15,16 +18,16 @@ export async function getServerSideProps() {
 const Home = ({
   todos,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(todos);
-
   return (
     <>
+      <h1 className="text-center text-2xl font-bold uppercase mb-3 text-gradient">
+        Todo List
+      </h1>
       <div>
         <TodoCard>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
+          {todos.map((todo) => (
+            <TodoItem key={todo._id} todo={todo} />
+          ))}
         </TodoCard>
       </div>
     </>
